e7c8672ac02c5369e93f12656d2d82f55dcb04da
chaeni <cogus080520@gmail.com>
2021-08-30 17:50:22 +0900

    spring

---
+++ src/main/java/com/iu/c1/bankbook/BankbookController.java
@@ -0,0 +1,28 @@
+package com.iu.c1.bankbook;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+@RequestMapping("/bankbook/*")
+public class BankbookController {
+	//pojo( plain Old Java Object)
+	
+	@RequestMapping(value = "bankbookList.do", method = RequestMethod.GET)
+	public String list() {
+		System.out.println("bankbook list");
+		
+		return "bankbook/bankbookList";
+	}
+	
+	@RequestMapping("bankbookSelect.do")
+	public String select(int num, String name) {
+		
+		System.out.println("Value : "+num);
+		System.out.println("Name : "+name);
+		return "bankbook/bankbookSelect";
+	}
+}
